441e0336da464acec0ef4f0612b6f0a81cdff346
Helio <helio.hetfield@gmail.com>
2017-11-15 08:37:22 -0200

    2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(allowCrossDomain);
+
 app.use((req, res, next) => {
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
